1493853f4405bae0084025a78d371b9a8d6478d9
Edwin Jay Javier <javier.edwinjay@gmail.com>
2019-07-26 13:47:32 +0000

    Additional changes

--- src/main/java/com/winj/App.java
+++ src/main/java/com/winj/App.java
@@ -4,10 +4,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import io.jooby.Jooby;
+import io.jooby.MediaType;
 import io.jooby.json.JacksonModule;
 
 
 import static com.winj.EbeanService.*;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class App extends Jooby {
     
@@ -20,14 +23,28 @@ public class App extends Jooby {
     /** ebean */
 	new EbeanBootstrap();
     
+    ObjectMapper mapper = new ObjectMapper();
+
+      decoder(MediaType.json, (ctx, type) -> {          
+    
+        byte[] body = ctx.body().bytes();              
+        
+        //mapper.readValue(body, type);
+        return null;
+        //return lib.fromJson(body, type);               
+      });
+    
     get("/", ctx -> "Welcome to Jooby!");
     
     get("/customers", ctx -> findAll(Customer.class, customers -> customers));
     
     get("/customer/{id}", ctx ->
-        findOne(Customer.class, ctx.path("id").longValue(), customer -> customer));
+        findOne(Customer.class, ctx.path("id").longValue(), customer -> {
+            customer.setName("namu");
+            return customer;
+        }));
     
-    post("/customer", ctx -> insert(ctx.body().to(Customer.class), message -> message));
+    post("/customer", ctx -> insert(ctx.body(Customer.class), message -> message));
   }
 
   public static void main(String[] args) {
